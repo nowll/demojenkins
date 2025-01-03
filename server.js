acbea88bd9f3580c77549a71741b1e5389749246
edosyhptra <edo23syahputra@gmail.com>
2025-01-03 16:30:44 +0700

    edit code

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World - from PNT Class! v8.0.0');
+  res.send('Hello World - from PNT Class! v10.0.0');
 });
 
 app.listen(PORT, HOST, () => {
